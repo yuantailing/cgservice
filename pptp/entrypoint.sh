4061348b0d09be734fdb119dd777f2a3095bbfb9
Tailing Yuan <yuantailing@gmail.com>
2018-10-13 18:26:13 +0800

    add pptp to cgserver, remove restrictions on PPTP and L2TP traffic

--- pptp/entrypoint.sh
+++ pptp/entrypoint.sh
@@ -11,10 +11,7 @@ iptables -A FORWARD -s 192.168.61.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK
 
 # redirect net.tsinghua.edu.cn
 netredirect=$(nslookup netredirect | awk -F': ' 'NR==6 {print $2}')
-iptables -t nat -A PREROUTING -d 166.111.4.0/24 -j DNAT --to ${netredirect}
-iptables -t nat -A PREROUTING -d 166.111.120.0/24 -j DNAT --to ${netredirect}
-iptables -t nat -A PREROUTING -d 166.111.204.0/24 -j DNAT --to ${netredirect}
-iptables -t nat -A PREROUTING -d 59.66.0.0/16 -j DNAT --to ${netredirect}
-iptables -t nat -A PREROUTING -d 101.6.0.0/16 -j DNAT --to ${netredirect}
+iptables -t nat -A PREROUTING -d 166.111.204.120 -j DNAT --to ${netredirect}
+iptables -t nat -A PREROUTING -d 101.6.4.100 -j DNAT --to ${netredirect}
 
 exec "$@"
