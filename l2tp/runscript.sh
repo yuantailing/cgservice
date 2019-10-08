5b927e97da54c175bdc9c7a92293ade631291fef
Tailing Yuan <yuantailing@gmail.com>
2019-10-08 16:51:58 +0800

    restrict VPN user IP

--- l2tp/runscript.sh
+++ l2tp/runscript.sh
@@ -10,6 +10,15 @@ mkdir -p /var/run/xl2tpd
 iptables -t nat -A POSTROUTING -s 192.168.46.0/23 ! -d 192.168.46.0/23 -j MASQUERADE
 iptables -t mangle -A FORWARD -s 192.168.46.0/23 -p tcp -m tcp --tcp-flags SYN,ACK,FIN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
+# Tsinghua IP only
+iptables -A INPUT -s 166.111.0.0/16 -j ACCEPT
+iptables -A INPUT -s 59.66.0.0/16 -j ACCEPT
+iptables -A INPUT -s 101.5.0.0/16 -j ACCEPT
+iptables -A INPUT -s 101.6.0.0/16 -j ACCEPT
+iptables -A INPUT -s 183.172.0.0/15 -j ACCEPT
+iptables -A INPUT -p udp --dport 500 -j DROP
+iptables -A INPUT -p udp --dport 4500 -j DROP
+
 # redirect net.tsinghua.edu.cn
 netredirect=$(nslookup netredirect | awk -F': ' 'NR==6 {print $2}')
 iptables -t nat -A PREROUTING -d 166.111.204.120 -j DNAT --to ${netredirect}
--- radius/conf/fetch_nt_psw.py
+++ radius/conf/fetch_nt_psw.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import requests
 import settings
 import sys
@@ -7,11 +8,15 @@ import sys
 if __name__ == '__main__':
     username = os.environ.get('USER_NAME').strip('"')
     nas_ip = os.environ.get('NAS_IP_ADDRESS')
+    ip = os.environ.get('CALLING_STATION_ID')
+    if ip:
+        ip = re.match(r'"?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(\[\d+\])?"?', ip).group(1)
     res = requests.post(
         settings.API_URL,
         data=dict(
             username=username,
             nas_ip=nas_ip,
+            ip=ip,
             key='NT-Password',
             api_secret=settings.API_SECRET,
         )
