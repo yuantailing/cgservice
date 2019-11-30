fd809584d613bfd5e6df1de32e2959c994facdf9
Tailing Yuan <yuantailing@gmail.com>
2019-11-30 18:36:40 +0800

    fix backup_aws.sh

--- backup/backup_aws.sh
+++ backup/backup_aws.sh
@@ -19,13 +19,13 @@ xz -0 dump.tar
 xz -0 sharelatex.tar
 xz -0 -c /mnt/redis/dump.rdb >dump.rdb.xz
 
-openssl rand 32 >/dev/shm/aes_key
-openssl rsautl -encrypt -in /dev/shm/aes_key -pubin -inkey <(echo "-----BEGIN PUBLIC KEY-----"; printenv BACKUP_PUBKEY; echo "-----END PUBLIC KEY-----") -out bucket/aes_key.enc
-openssl enc -e -aes-256-cbc -in all-databases.sql.xz -kfile /dev/shm/aes_key -out bucket/mysql/all-databases.sql.xz.enc
-openssl enc -e -aes-256-cbc -in dump.tar.xz -kfile /dev/shm/aes_key -out bucket/mongo/dump.tar.xz.enc
-openssl enc -e -aes-256-cbc -in sharelatex.tar.xz -kfile /dev/shm/aes_key -out bucket/sharelatex/sharelatex.tar.xz.enc
-openssl enc -e -aes-256-cbc -in dump.rdb.xz -kfile /dev/shm/aes_key -out bucket/redis/dump.rdb.xz.enc
-rm -f /dev/shm/aes_key
+openssl rand 32 >aes_key
+openssl rsautl -encrypt -in aes_key -pubin -inkey <(echo "-----BEGIN PUBLIC KEY-----"; printenv BACKUP_PUBKEY; echo "-----END PUBLIC KEY-----") -out bucket/aes_key.enc
+openssl enc -e -aes-256-cbc -in all-databases.sql.xz -kfile aes_key -out bucket/mysql/all-databases.sql.xz.enc
+openssl enc -e -aes-256-cbc -in dump.tar.xz -kfile aes_key -out bucket/mongo/dump.tar.xz.enc
+openssl enc -e -aes-256-cbc -in sharelatex.tar.xz -kfile aes_key -out bucket/sharelatex/sharelatex.tar.xz.enc
+openssl enc -e -aes-256-cbc -in dump.rdb.xz -kfile aes_key -out bucket/redis/dump.rdb.xz.enc
+rm -f aes_key
 
 s3cp() {
     aws --cli-read-timeout=30 --cli-connect-timeout=5 --region="${AWS_S3_REGION}" s3 cp --recursive bucket/ s3://"${AWS_S3_BUCKET}"/
@@ -33,7 +33,7 @@ s3cp() {
 
 s3cp || s3cp || s3cp
 
-rm -rf bucket/s3workspace/
+rm -rf /bucket/s3workspace/
 
 # How to decrypt
 #openssl rsautl -decrypt -in aes_key.enc -inkey id_rsa.pem -out aes_key
